d862c14a9b3543dc539f991b63399529a2b161cc
Nickel 13 <nikesushko@gmail.com>
2018-08-19 14:27:44 +0300

    draft version

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const 
-    gulp = require('gulp-4.0.build'),
-    pug = require('gulp-pug'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+const   gulp = require('gulp'),
+        pug = require('gulp-pug'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        del = require('del'),
+        server = require('browser-sync');
 
 gulp.task('pug', () => {
     return gulp.src('src/pug/**/*.pug')
@@ -13,5 +14,38 @@ gulp.task('pug', () => {
 gulp.task('sass', () => {
     return gulp.src('src/sass/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('build'))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build/css'))
 })
+
+gulp.task('img', () => {
+    return gulp.src('src/img/**/*.*')
+        .pipe(gulp.dest('build/img'))
+})
+
+gulp.task('server', () => {
+    server.init({
+        server: {
+          baseDir: 'build'
+        },
+        notify: true
+      })
+})
+
+gulp.task('watch', function () {
+    gulp.watch('src/sass/**/*.*', gulp.series('sass'));
+    gulp.watch('src/pug/**/*.*', gulp.series('pug'));
+    // gulp.watch('src/js/**/*.*', gulp.series('js'));
+  
+    gulp.watch('build/**/*.*').on('change', server.reload);
+  });
+
+  gulp.task('clean', function () {
+    return del('build');
+  });
+  
+  gulp.task('build', gulp.series('clean', gulp.parallel('pug', 'sass', 'img')));
+  
+  gulp.task('serve', gulp.parallel('watch', 'server'));
+  
+  gulp.task('dev', gulp.series('build', 'serve'));
